65418ea11390f9dea668f5028318135a9d5051d4
Yves Lasnier <yves.lasnier@gmail.com>
2017-10-11 11:16:13 +0200

    update: workstation commit chg: vim theme fix: steam launcher

--- linux/bin/steam-bootstrap.sh
+++ linux/bin/steam-bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-LD_LIBRARY_PATH="/usr/lib/mesa-diverted/x86_64-linux-gnu:/usr/lib/mesa-diverted/i386-linux-gnu/:$LD_LIBRARY_PATH" steam
+#LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6'
+#LD_PRELOAD='/usr/lib/x86_64-linux-gnu/dri/i965_dri.so /usr/lib/x86_64-linux-gnu/dri/swrast_dri.so' \
+LD_LIBRARY_PATH="/usr/lib/mesa-diverted/x86_64-linux-gnu:/usr/lib/mesa-diverted/i386-linux-gnu/:/home/yl/.steam-lib/:$LD_LIBRARY_PATH" \
+STEAM_RUNTIME=0 \
+steam
 
